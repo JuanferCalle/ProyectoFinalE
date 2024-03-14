316369f39cd5a087c05bb3ed2edfb23f0b0315b0
JuanferCalle <calle.juan@correounivalle.edu.co>
2024-03-14 16:43:09 -0500

    Ahora solo salen los datos del año 2023

--- main.R
+++ main.R
@@ -1,7 +1,27 @@
 library(readr)
+library(readxl)
+library(git2r)
+
+##Datos sacados de https://www.kaggle.com/datasets/malingarajapaksha/dataset
+
+##Cargamos los datos desde nuestro repositorio de GitHub
+datos <-read.csv("https://raw.githubusercontent.com/JuanferCalle/ProyectoFinalE/main/Dataset.csv")
 
 
-read.csv("https://raw.githubusercontent.com/JuanferCalle/ProyectoFinalE/main/Dataset.csv")
+# Filtramos los datos donde work_year es diferente a 2023
+añoDescartable <- subset(datos, work_year != 2023)
 
+
+##Eliminamos los datos que tengan un año diferente a 2023, dado a los fines de investigación del proyecto
+datos_filtrados <- subset(datos, !(work_year %in% c(2020, 2021, 2022)))
+
+# Calculamos el número de valores faltantes en cada columna
+valores_faltantes <- colSums(is.na(datos))
+
+# Identificamos las columnas que tienen valores faltantes
+columnas_con_faltantes <- names(valores_faltantes[valores_faltantes > 0])
+
+# Mostramos las columnas con valores faltantes
+print(columnas_con_faltantes)
 
 
